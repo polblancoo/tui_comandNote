6f4cdb9c5717e394846ba348b665627fcff072e4
polblancoo <polblancoo@gmail.com>
2024-12-27 00:31:18 -0300

    Agregar exportacion y cambio de path/data-(2)

--- build.rs
+++ build.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
 fn main() {
     println!("cargo:rerun-if-changed=src/data/default.json");
+    
+    // Copiar default.json al directorio de salida
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("default.json");
+    fs::copy("src/data/default.json", dest_path).unwrap();
 } 
